cd489f6337129afd33ee7a08879767adef3eedc8
Alataq <72706324+Alataq@users.noreply.github.com>
2025-04-24 12:25:29 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload/preload.js')
+    },
+    
+  })
+
+  win.removeMenu()
+  win.loadFile('app/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+    process.exit(0)
+  }
+})
